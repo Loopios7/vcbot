e4b73431df9019b183f10015ade9002518b3ba5c
Dan <dan@loopsites.com>
2020-09-21 23:23:35 +0200

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,5 +1,8 @@
-# The channel to send the log messages to. I won't need ths I need the role tocreate on init/join and assign instead
-CHANNEL_NAME = "general"
-
 # The bot token
-BOT_TOKEN = "BOT_TOKEN"
+BOT_TOKEN = "INSERT TOKEN"
+
+# Role name
+ROLE_NAME = "Voice User"
+
+# Owner ID
+OWNER_ID = 0
